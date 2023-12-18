234cf60ff0dddffa21f18d63cde52af9b8e570e5
aandrewchen <andche@ucdavis.edu>
2023-12-18 11:47:48 -0800

    Deleted original AF controller and route

--- routes/activities.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import ActivitiesController from '../controllers/activities.js';
-
-const router = express.Router();
-
-router.get('/', ActivitiesController.getActivities);
-
-export default router;
