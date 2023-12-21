f198ecb3d3f852c3e6f1dfac2bee0c7b03822bd2
aandrewchen <andche@ucdavis.edu>
2023-12-21 11:14:08 -0800

    Edited rssActivities schema and model

--- model/rssActivities.js
+++ model/rssActivities.js
@@ -2,20 +2,38 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const rssActivitiesSchema = new Schema({
-    activity: {
-        actor: {
-            author: {
-                displayName: String,
-            },
+    title: String,
+    object: {
+        content: String,
+        objectType: String,
+        ucdSrcId: String,
+        ucdEdusModel: {
+            url: String,
+            urlDisplayName: String,
         },
-        title: String,
-        object: {
-            content: String,
-            ucdEdusModel: {
-                url: String,
-            },
-        },
+        id: String,
+        masterId: String,
     },
+    ucdEdusMeta: {
+        startDate: String,
+        labels: [String],
+        endDate: String,
+    },
+    verb: String,
+    actor: {
+        id: String,
+        displayName: String,
+        author: {
+            id: String,
+            displayName: String,
+        },
+        objectType: String,
+    },
+    icon: String,
+    id: String,
+    priority: Number,
+    published: String,
+    score: Number,
 });
 
 const rssActivities = model("rssActivities", rssActivitiesSchema);
