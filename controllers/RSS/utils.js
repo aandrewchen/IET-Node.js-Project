6ec09f0790edd554198c122a1c6dc3979c3a9137
aandrewchen <andche@ucdavis.edu>
2024-01-02 16:24:46 -0800

    Added orderActivities function

--- controllers/RSS/utils.js
+++ controllers/RSS/utils.js
@@ -19,14 +19,17 @@ export function addAggieFeedProperties(rssActivity) {
     const icon = "NOT SURE YET";
 
     rssActivity.object = {
-        ...rssActivity.object,
+        content: rssActivity.object.content,
         objectType: "notification",
+        ucdSrcId: rssActivity.object.ucdSrcId,
+        ucdEdusModel: {
+            ...rssActivity.object.ucdEdusModel,
+        },
         id: objectId,
         masterId: objectMasterId,
     };
 
     rssActivity.ucdEdusMeta = {
-        ...rssActivity.ucdEdusMeta,
         startDate: startDate,
         labels: labels,
         endDate: "2030-01-01T00:00:00.000Z",
@@ -37,8 +40,8 @@ export function addAggieFeedProperties(rssActivity) {
         id: actorId,
         displayName: actorDisplayName,
         author: {
-            ...rssActivity.actor.author,
-            id: authorId
+            id: authorId,
+            displayName: rssActivity.actor.displayName,
         },
         objectType: "organization",
     };
@@ -49,4 +52,20 @@ export function addAggieFeedProperties(rssActivity) {
     rssActivity.verb = "post";
 
     return rssActivity;
+}
+
+export function orderActivities(activities) {
+    return activities.map(activity => ({
+        _id: activity._id,
+        title: activity.title,
+        object: activity.object,
+        ucdEdusMeta: activity.ucdEdusMeta,
+        verb: activity.verb,
+        actor: activity.actor,
+        icon: activity.icon,
+        id: activity.id,
+        priority: activity.priority,
+        published: activity.published,
+        score: activity.score,
+    }));
 }
